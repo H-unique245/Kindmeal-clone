0de4106114f951871b02ae0b54e9f09563f88a7c
H-unique245 <a.hemant96@gmail.com>
2022-09-27 15:37:06 +0530

    Initial commit

---
+++ public-language-8892-kindmeal-clone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kind Meal clone </h1>
+      <h2>HOME PAGE </h2>
+    </div>
+  );
+}
+
+export default App;
